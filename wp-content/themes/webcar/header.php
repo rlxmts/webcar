7bad69a71cba7bef1f5ccdc1dccf22ddd76ad710
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-09-30 21:35:10 -0300

    :lipstick: Estilizando body e menu

--- wp-content/themes/webcar/header.php
+++ wp-content/themes/webcar/header.php
@@ -5,16 +5,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo( show: 'name'); ?></title>
   <?php wp_head(); ?>
-</head>
-<body>
   
-<?php
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  
+  <link rel='stylesheet' href="<?= get_template_directory_uri() . '/css/reset.css'?>" />
+  <link rel='stylesheet' href="<?= get_template_directory_uri() . '/css/header.css'?>" />
 
-the_custom_logo();
+</head>
+<body <?php body_class(); ?>>
+  <header>
+    <?php
+    the_custom_logo();
+    wp_nav_menu(
+      array(
+        'menu' => 'menu-navegacao'
+      )
+    );
+    ?>
+  </header>
 
-wp_nav_menu(
-  array(
-    'menu' => 'menu-navegacao'
-  )
-);
-?>
+
+
+

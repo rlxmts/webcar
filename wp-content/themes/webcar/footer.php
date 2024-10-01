cd456cc4589c9b189e8af4e3f976ca1847f9bc3a
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-10-01 10:45:16 -0300

    :lipstick: Criando e estilizando Footer

--- wp-content/themes/webcar/footer.php
+++ wp-content/themes/webcar/footer.php
@@ -1,3 +1,8 @@
+<footer class="rodape">
+  <p>
+    &copy; Todos os direitos reservados à <?php echo get_bloginfo('name'); ?> <?= date(format:'Y'); ?>
+  </p>
+</footer>
 </body>
 <?php wp_footer(); ?>
 </html>
